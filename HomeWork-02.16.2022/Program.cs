999069c56d84d8b08119adfadd12b71c33b99710
sadiqli1 <isaas@code.edu.az>
2022-02-16 07:41:16 +0400

    created folde

---
+++ HomeWork-02.16.2022/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HomeWork_02._16._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "HeLlO wOrLd HoW aRe You";
+            string newstr;
+            foreach (char letter in str)
+            {
+                if (char.IsUpper(letter))
+                { 
+                    char.ToLower(letter);
+                    string newstr = (string)letter.Clone;
+                }
+                else if (char.IsLower(letter))
+                {
+                    char.ToUpper(letter);
+                    string newstr = (string)letter.Clone;
+                }
+                Console.WriteLine(newstr);
+            }
+
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    if (letter=char.IsUpper(i))
+            //    {
+
+            //    }
+
+            //}
+        }
+    }
+}
